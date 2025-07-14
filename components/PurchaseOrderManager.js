3a02a584e6d9d6372eb1bd653bad3cbcc709e6fd
MartinDominique <servicestmt@gmail.com>
2025-07-14 15:00:25 -0400

    Update PurchaseOrderManager.js

--- components/PurchaseOrderManager.js
+++ components/PurchaseOrderManager.js
@@ -410,13 +410,6 @@ export default function PurchaseOrderManager({ user }) {
                 <Upload className="w-5 h-5 mr-2" />
                 {loading ? 'Envoi...' : 'Envoyer Rapport PDF'}
               </button>
-              <button
-                onClick={handleLogout}
-                className="inline-flex items-center px-4 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700"
-              >
-                <LogOut className="w-5 h-5 mr-2" />
-                Déconnexion
-              </button>
             </div>
           </div>
         </div>
