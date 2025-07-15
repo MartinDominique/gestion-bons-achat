ae69ac05092ae6ad26e2c0ea1297e00769b2de62
MartinDominique <servicestmt@gmail.com>
2025-07-14 22:28:57 -0400

    Delete app/soumissions/page.js

--- app/soumissions/page.js
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import React from 'react';
-import SoumissionsManager from '../../components/SoumissionsManager';
-
-export default function SoumissionsPage() {
-  // On peut passer "user" si nécessaire ; sinon le composant gère l'absence
-  return <SoumissionsManager />;
-}
