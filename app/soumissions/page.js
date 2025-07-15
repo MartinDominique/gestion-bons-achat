fa9f7c1c09eb7304b9ade3c8a85e0df2abc3c62f
MartinDominique <servicestmt@gmail.com>
2025-07-14 21:22:45 -0400

    Create page.js
    feat: add /soumissions route

---
+++ app/soumissions/page.js
@@ -0,0 +1,9 @@
+'use client';
+
+import React from 'react';
+import SoumissionsManager from '../../components/SoumissionsManager';
+
+export default function SoumissionsPage() {
+  // On peut passer "user" si nécessaire ; sinon le composant gère l'absence
+  return <SoumissionsManager />;
+}
