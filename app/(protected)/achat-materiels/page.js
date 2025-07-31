b1963ebebc571542d1c051a26b342751697753a3
MartinDominique <servicestmt@gmail.com>
2025-07-31 17:11:18 -0400

    Create page.js

---
+++ app/(protected)/achat-materiels/page.js
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import SupplierPurchaseManager from '../../../components/SupplierPurchaseManager';
+
+export default function AchatMaterielsPage() {
+  return <SupplierPurchaseManager />;
+}
