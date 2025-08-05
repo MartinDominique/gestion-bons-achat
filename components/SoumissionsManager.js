3027d9988b0c1c85100aa17872d71d4ad9555fae
MartinDominique <servicestmt@gmail.com>
2025-08-04 21:54:57 -0400

    Update SoumissionsManager.js

--- components/SoumissionsManager.js
+++ components/SoumissionsManager.js
@@ -759,13 +759,20 @@ export default function SoumissionsManager() {
               display: none !important;
             }
           }
+          @media screen {
+  .print-area,
+  .print-area-client {
+    display: none;
+  }
+}
+
           `}
         </style>
 
         <div className="max-w-6xl mx-auto p-4">
           {/* VERSION COMPLÈTE AVEC COÛTS - Zone d'impression */}
           {selectedItems.length > 0 && (
-            <div className="print-area" style={{ display: 'none' }}>
+            <div className="print-area">
               <div className="print-header">
                 <div className="print-company-info">
                   <strong>Services TMT Inc.</strong><br />
@@ -837,7 +844,7 @@ export default function SoumissionsManager() {
 
           {/* VERSION CLIENT SANS COÛTS - Zone d'impression */}
           {selectedItems.length > 0 && (
-            <div className="print-area-client" style={{ display: 'none' }}>
+            <div className="print-area-client">
               <div className="print-header">
                 <div className="print-company-info">
                   <strong>Services TMT Inc.</strong><br />
