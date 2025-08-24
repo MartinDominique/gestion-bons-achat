87ae56588d6585dccddca10a48cba37a61051112
MartinDominique <servicestmt@gmail.com>
2025-08-23 20:47:17 -0400

    Update DeliveryDashboard.js

--- components/DeliveryDashboard.js
+++ components/DeliveryDashboard.js
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../lib/supabase';
 import { Package, Clock, CheckCircle, AlertTriangle, Eye, Truck, Calendar, Filter } from 'lucide-react';
