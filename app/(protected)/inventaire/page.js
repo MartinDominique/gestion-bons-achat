25c8ce6d730573ed54b797fa8a8cdae28d277428
MartinDominique <servicestmt@gmail.com>
2025-08-27 11:24:17 -0400

    Create page.js

---
+++ app/(protected)/inventaire/page.js
@@ -0,0 +1,7 @@
+'use client';
+import React from 'react';
+import InventoryManager from '../../../components/InventoryManager';
+
+export default function InventairePage() {
+  return <InventoryManager />;
+}
