354c7a92c3143c4f56b20d1ce339fa267c5c8b78
MartinDominique <servicestmt@gmail.com>
2025-09-04 22:23:39 -0400

    Update PurchaseOrderModal.js

--- components/PurchaseOrderModal.js
+++ components/PurchaseOrderModal.js
@@ -1972,8 +1972,7 @@ const startAddingNewItem = () => {
             </div>
           </div>
         </div>
-      </div>
-
+   
       {/* Modal import soumissions */}
       {showSubmissionModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
