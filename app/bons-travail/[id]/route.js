84e15f6a9eea6ebb39063de8f9ec5df2bc7681d1
MartinDominique <servicestmt@gmail.com>
2025-09-09 21:58:30 -0400

    Update route.js

--- app/bons-travail/[id]/route.js
+++ app/bons-travail/[id]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { createClient } from '../../../../lib/supabase';
+import { createClient } from '../../../lib/supabase';
 
 // GET - Récupérer un bon de travail spécifique
 export async function GET(request, { params }) {
