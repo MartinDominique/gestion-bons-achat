75b59a54ce72cb8d2970a76599561efe373ebe02
MartinDominique <servicestmt@gmail.com>
2025-10-17 17:38:22 -0400

    Update route.js

--- app/api/clients/route.js
+++ app/api/clients/route.js
@@ -1,3 +1,7 @@
+///////////////////////////////////////////
+//app/api/clients/route.js
+/////////////////////////////////////////
+
 import { NextResponse } from 'next/server';
 import { createClient } from '../../../lib/supabase';
 
