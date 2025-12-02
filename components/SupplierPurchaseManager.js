2bc5dbe86992202b6d08256cdc7db4a081c5c8d0
MartinDominique <servicestmt@gmail.com>
2025-12-01 19:47:41 -0500

    Update SupplierPurchaseManager.js

--- components/SupplierPurchaseManager.js
+++ components/SupplierPurchaseManager.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SupplierReceiptModal from './SupplierReceiptModal';
 import { 
   MoreVertical, Edit, Trash2, Search, Plus, ShoppingCart, Building2, Wrench, Calendar, Truck
