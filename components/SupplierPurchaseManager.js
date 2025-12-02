9f79eef97c0d6b121b790358ab56bfbfa78a5bcc
MartinDominique <servicestmt@gmail.com>
2025-12-01 19:41:54 -0500

    Update SupplierPurchaseManager.js

--- components/SupplierPurchaseManager.js
+++ components/SupplierPurchaseManager.js
@@ -131,38 +131,24 @@ export default function SupplierPurchaseManager() {
     handleImportSelectedItems,
     
      // Fonctions utilitaires
-      resetForm,
-      resetSupplierForm,
-      resetAddressForm,
-      shouldShowBilingual,
-      isCanadianSupplier,
-      handleFixExistingPurchases,
-      handleTestEmail,
-      loadSupplierPurchases,
-      
-      // Données filtrées
-      filteredPurchases
-    } = hookData;
-  
-    // États pour le modal de réception
-    const [showReceiptModal, setShowReceiptModal] = useState(false);
-    const [selectedPurchaseForReceipt, setSelectedPurchaseForReceipt] = useState(null);
-  
-    // Handler pour ouvrir le modal de réception
-    const openReceiptModal = (purchase) => {
-      setSelectedPurchaseForReceipt(purchase);
-      setShowReceiptModal(true);
-    };
+    resetForm,
+    resetSupplierForm,
+    resetAddressForm,
+    shouldShowBilingual,
+    isCanadianSupplier,
+    handleFixExistingPurchases,
+    handleTestEmail,
+    loadSupplierPurchases,
     
     // Données filtrées
-    filteredPurchases,
-    loadSupplierPurchases
+    filteredPurchases
   } = hookData;
 
   // États pour le modal de réception
   const [showReceiptModal, setShowReceiptModal] = useState(false);
   const [selectedPurchaseForReceipt, setSelectedPurchaseForReceipt] = useState(null);
 
+  // Handler pour ouvrir le modal de réception
   const openReceiptModal = (purchase) => {
     setSelectedPurchaseForReceipt(purchase);
     setShowReceiptModal(true);
